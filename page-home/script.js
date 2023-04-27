6f4924dc6b2156920ccacb735f655fdcab6846e3
GabrielBismark <gbismark1405@gmail.com>
2023-04-26 21:54:26 -0300

    ajeitando coisas

--- page-home/script.js
+++ page-home/script.js
@@ -2,7 +2,7 @@ function initAnimacaoScroll() {
     const sections = document.querySelectorAll('.js-scroll');
 
     if(sections.length) {
-        const windowMetade = window.innerHeight * 0.6;
+        const windowMetade = window.innerHeight * 0.9;
 
         function animaScroll(){
             sections.forEach((section) =>{
